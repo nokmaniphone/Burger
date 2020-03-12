a037aad05541aa594d17de64b039cef654def432
nokmaniphone <nokmaniphonesayavong@gmail.com>
2020-03-12 00:51:36 -0700

    install npm packge and create config

---
+++ config/index.js
@@ -0,0 +1,20 @@
+const { createConnection } = require('mysql2')
+
+let connection
+
+if (process.env.JAWSDB_URL) {
+  connection = createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '55610096',
+    database: 'burgers_db'
+  })
+}
+
+
+
+
+module.exports = connection
